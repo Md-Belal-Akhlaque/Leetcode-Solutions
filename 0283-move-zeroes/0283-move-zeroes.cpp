e973f722e6c67e0b8848aa6cac79fdba57ce7a94
Md. Belal Akhlaque <mdbelalmzp01234@gmail.com>
2026-07-21 01:48:52 +0530

    Time: 0 ms (100%), Space: 23.8 MB (56.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,20 +1,13 @@
 class Solution {
-    public:
-    void swap (int &a,int &b){
-        int temp = a;
-        a = b;
-        b = temp;
-    }
 public:
     void moveZeroes(vector<int>& nums) {
-        int j =0;
-        for(int i =0; i<nums.size();i++){
+        int j = 0;
 
-            if(nums[i]!=0){
-                swap(nums[j],nums[i]);
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
                 j++;
-            }            
+            }
         }
-
     }
 };
