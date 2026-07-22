af53119d0aefc1e27c3746b0144e96da0c486799
Md. Belal Akhlaque <mdbelalmzp01234@gmail.com>
2026-07-22 20:19:20 +0530

    Time: 3 ms (45.98%), Space: 9.9 MB (26.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -20,3 +20,26 @@ public:
         return true;
     }
 };
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.size() != t.size())
+//             return false;
+
+//         int freq[26] = {0};
+
+//         for (char ch : s)
+//             freq[ch - 'a']++;
+
+//         for (char ch : t)
+//             freq[ch - 'a']--;
+
+//         for (int x : freq) {
+//             if (x != 0)
+//                 return false;
+//         }
+
+//         return true;
+//     }
+// };
