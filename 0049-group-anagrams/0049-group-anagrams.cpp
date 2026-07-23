c4c33cc328142e1a9f9dcc702e0513a393908422
Md. Belal Akhlaque <mdbelalmzp01234@gmail.com>
2026-07-23 08:43:54 +0530

    Time: 16 ms (59.39%), Space: 24.9 MB (80.14%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+
+        for(auto s : strs){
+            string key = s;        // first letter 
+            sort(key.begin(),key.end());
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> res;
+        for(auto &it : mp){
+            res.push_back(it.second);
+        }
+
+        return res;
+    }
+};
